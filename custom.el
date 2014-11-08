b72387784e6838b584c712d051f90afc2d5b51d7
Nicolas <nicolas.rolland@gmail.com>
2014-11-08 10:42:44 +0100

    completed some missing files

---
+++ custom.el
@@ -0,0 +1,35 @@
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 180 :width normal :foundry "nil" :family "Monaco"))))
+ '(shm-current-face ((t (:background "#eee8d5"))))
+ '(shm-quarantine-face ((t (:background "lemonchiffon")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-autoface-mode nil)
+ '(aquamacs-customization-version-id 305 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(default-frame-alist
+    (quote
+     ((tool-bar-lines . 0)
+      (menu-bar-lines . 1)
+      (foreground-color . "Black")
+      (background-color . "White")
+      (cursor-type . box)
+      (cursor-color . "Red")
+      (vertical-scroll-bars . right)
+      (internal-border-width . 0)
+      (left-fringe . 1)
+      (right-fringe)
+      (fringe))))
+ '(ns-tool-bar-display-mode nil t)
+ '(ns-tool-bar-size-mode nil t)
+ '(shm-program-name "
+    " t)
+ '(visual-line-mode nil t))
