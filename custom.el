23a4de08822dbb914cf539ab0e1255254990e4cd
Nicolas Rolland <nicolas.rolland@gmail.com>
2015-05-25 22:09:21 +0100

    color code in org

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "Monaco"))))
  '(shm-current-face ((t (:background "#eee8d5"))))
  '(shm-quarantine-face ((t (:background "lemonchiffon")))))
 (custom-set-variables
@@ -14,33 +14,61 @@
  '(agda2-include-dirs
    (quote
     ("." "/Users/nrolland/clones/agda-stdlib/src" "/Users/nrolland/clones/HoTT-Agda/lib")))
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-autoface-mode nil)
  '(aquamacs-customization-version-id 307 t)
  '(aquamacs-tool-bar-user-customization nil t)
- '(bmkp-last-as-first-bookmark-file "~/hask/gist/bookmark")
+ '(bmkp-last-as-first-bookmark-file "~/.emacs.d/bookmarks")
+ '(custom-enabled-themes (quote (misterioso)))
+ '(custom-safe-themes
+   (quote
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(default-frame-alist
     (quote
      ((tool-bar-lines . 0)
       (menu-bar-lines . 1)
-      (foreground-color . "Black")
-      (background-color . "White")
       (cursor-type . box)
-      (cursor-color . "Red")
       (vertical-scroll-bars . right)
       (internal-border-width . 0)
       (left-fringe . 1)
       (right-fringe)
       (fringe))))
+ '(default-input-method "Agda")
+ '(fci-rule-color "#14151E")
  '(global-linum-mode t)
  '(haskell-hoogle-command "hoogle")
  '(haskell-process-log t)
  '(ns-tool-bar-display-mode (quote both) t)
  '(ns-tool-bar-size-mode nil t)
+ '(org-src-fontify-natively t)
  '(proof-three-window-enable t)
  '(purescript-mode-hook
    (quote
     (turn-on-purescript-simple-indent inferior-psci-mode)))
  '(shm-program-name "
     " t)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#d54e53")
+     (40 . "goldenrod")
+     (60 . "#e7c547")
+     (80 . "DarkOliveGreen3")
+     (100 . "#70c0b1")
+     (120 . "DeepSkyBlue1")
+     (140 . "#c397d8")
+     (160 . "#d54e53")
+     (180 . "goldenrod")
+     (200 . "#e7c547")
+     (220 . "DarkOliveGreen3")
+     (240 . "#70c0b1")
+     (260 . "DeepSkyBlue1")
+     (280 . "#c397d8")
+     (300 . "#d54e53")
+     (320 . "goldenrod")
+     (340 . "#e7c547")
+     (360 . "DarkOliveGreen3"))))
+ '(vc-annotate-very-old-color nil)
  '(visual-line-mode nil t))
